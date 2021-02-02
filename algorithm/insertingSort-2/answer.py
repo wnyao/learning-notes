f73e5ebe801464be80946e5e2cd82f8da8e1d87a
WenYaoKong <wenyao_k@hotmail.com>
2021-02-02 22:38:34 +0800

    Add new aweser for inserting sort 2

--- algorithm/insertingSort-2/answer.py
+++ algorithm/insertingSort-2/answer.py
@@ -1,12 +1,14 @@
-def insertionSort2(n, arr):
-  for i in range(1, n):
-    previous = arr[i - 1]
-    current = arr[i]
+# answer2 (personal attempt) ---------------------------------------
+
+def insertionSort1(n, arr):
+    for i in range(1, n):
+        previous = arr[i - 1]
+        current = arr[i]
 
     while previous > current:
-      # swap places (execution order is important)
-      arr[arr.index(current)] = previous
-      arr[arr.index(previous)] = current
+        # swap places (execution order is important)
+        arr[arr.index(current)] = previous
+        arr[arr.index(previous)] = current
 
       # handle index previous go beyond index 0
       if arr.index(current) - 1 < 0: break
@@ -16,3 +18,21 @@ def insertionSort2(n, arr):
       current = arr[arr.index(current)]
 
     print(" ".join(map(str, arr)))
+
+
+
+# answer2 ------------------------------------------------------
+# https://yourbasic.org/algorithms/loop-invariants-explained/
+
+def insertionSort2(n, arr):
+
+  for j in range(1, n):
+    key = arr[j]
+    i = j - 1
+
+    while i >= 0 and arr[i] > key:
+      arr[i + 1] = arr[i]
+      i = i - 1
+
+    arr[i + 1] = key
+    print(" ".join(map(str, arr)));
